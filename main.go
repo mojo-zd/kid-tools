0b1b5c9b80afe7b4dd40a382de6dc7e403bf8852
mojo <excessivespeed@126.com>
2016-11-03 17:40:28 +0800

    optimization

--- main.go
+++ main.go
@@ -7,7 +7,18 @@ import (
 	"math/rand"
 	"time"
 
+	"strings"
+
 	"github.com/golang/glog"
+)
+
+const (
+	CERT_SUFFIX = ".crt"
+	KEY_SUFFIX  = ".key"
+)
+
+var (
+	certfile, keyfile string
 )
 
 func main() {
@@ -15,17 +26,29 @@ func main() {
 	rand.Seed(time.Now().UnixNano())
 	glog.CopyStandardLogTo("INFO")
 
-	certFile := flag.Arg(0)
-	keyFile := flag.Arg(1)
-
-	if len(certFile) == 0 || len(keyFile) == 0 {
+	if len(flag.Args()) == 0 {
 		fmt.Println("请输入公钥和私钥的文件路径!")
 		return
 	}
-	pk, _, err := config.LoadCertAndKey(certFile, keyFile)
+
+	for _, ele := range flag.Args() {
+		if strings.HasSuffix(ele, CERT_SUFFIX) {
+			certfile = ele
+		} else if strings.HasSuffix(ele, KEY_SUFFIX) {
+			keyfile = ele
+		}
+	}
+
+	if len(certfile) == 0 || len(keyfile) == 0 {
+		fmt.Println("请输入公钥和私钥的文件路径!")
+		return
+	}
+
+	pk, _, err := config.LoadCertAndKey(certfile, keyfile)
 	if err != nil {
 		fmt.Println("生成公钥和私钥文件出错 %s", err)
 		return
 	}
+
 	fmt.Println(pk.KeyID())
 }
